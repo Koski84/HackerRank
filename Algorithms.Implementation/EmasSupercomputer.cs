ea12eceb623d4414448895db5fbbe6c4317da29a
Lamelas84 <lamelas84@gmail.com>
2017-12-30 02:21:01 +0100

    + EmasSupercomputer

---
+++ Algorithms.Implementation/EmasSupercomputer.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace Algorithms.Implementation
+{
+    public class EmasSupercomputer
+    {
+        private static int N;
+        private static int M;
+
+        private const int maximumPluses = 2;
+        private static int currentPluses = 0;
+
+        public static int[,] Matrix { get; private set; }
+
+        static void Main(String[] args)
+        {
+            string[] lineNM = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(lineNM[0]);
+            int m = Convert.ToInt32(lineNM[1]);
+
+            Init(n, m);
+
+            for(int i=0; i<N; i++)
+            {
+                string data = Console.ReadLine();
+                FillLine(i, data);
+            }
+
+            int result = Process();
+
+            Console.WriteLine(result);
+        }
+
+        public static void Init(int n, int m)
+        {
+            N = n;
+            M = m;
+            Matrix = new int[n,m];
+        }
+
+        public static void FillLine(int line, string data)
+        {
+            for (int i = 0; i < data.Length; i++)
+                Matrix[line, i] = data[i] == 'G' ? 0 : -1;
+        }
+
+        private static bool BelowIsBad(int i, int j)
+        {
+            i++;
+
+            return i == N || Matrix[i, j] == -1;
+        }
+
+        private static bool AboveIsBad(int i, int j)
+        {
+            i--;
+
+            return i < 0 || Matrix[i, j] == -1;
+        }
+
+        private static bool RightIsBad(int i, int j)
+        {
+            j++;
+
+            return j == M || Matrix[i, j] == -1;
+        }
+
+        private static bool LeftIsBad(int i, int j)
+        {
+            j--;
+
+            return j < 0 || Matrix[i, j] == -1;
+        }
+
+        private static bool IsBorder(int i, int j)
+        {
+            return i == 0 || j == 0 || i == N - 1 || j == M - 1;
+        }
+
+        public static int Process()
+        {
+            int maximumArea = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (Matrix[i, j] == 0)
+                    {
+                        int areaPlus = SetPlusAndGetHisMaximumArea(i, j);
+
+                        if (currentPluses < maximumPluses)
+                        {
+                            int areaAnotherPlus = Process();
+
+                            int multipliedArea = areaPlus * areaAnotherPlus;
+
+                            if (maximumArea < multipliedArea)
+                                maximumArea = multipliedArea;
+                        }
+                        else
+                        {
+                            if (maximumArea < areaPlus)
+                                maximumArea = areaPlus;
+                        }
+
+                        RemovePlus(i, j);
+                    }
+                }
+            }
+
+            return maximumArea;
+        }
+
+        public static int SetPlusAndGetHisMaximumArea(int i, int j)
+        {
+            int area = 1;
+            int c = 0;
+
+            do
+            {
+                if (!LeftIsBad(i, j - (1 * c)) 
+                    && !AboveIsBad(i - (1 * c), j) 
+                    && !RightIsBad(i, j + (1 * c)) 
+                    && !BelowIsBad(i + (1 * c), j))
+                {
+                    area += 4;
+                    SetSideCells(i, j, distance: ++c, value: -1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true);
+
+            Matrix[i, j] = area;
+            currentPluses++;
+
+            return area;
+        }
+
+        private static void SetSideCells(int i, int j, int distance, int value)
+        {
+            Matrix[i, j - (1 * distance)] = value;
+            Matrix[i - (1 * distance), j] = value;
+            Matrix[i, j + (1 * distance)] = value;
+            Matrix[i + (1 * distance), j] = value;
+        }
+
+        public static void RemovePlus(int i, int j)
+        {
+            int area = Matrix[i, j];
+
+            Matrix[i, j] = 0;
+
+            int c = 0;
+            while(area > 1)
+            {
+                area -= 4;
+                SetSideCells(i, j, distance: ++c, value: 0);
+            }
+
+            currentPluses--;
+        }
+    }
+}
---
+++ Algorithms.ImplementationTests/EmasSupercomputerTests.cs
@@ -0,0 +1,158 @@
+﻿using Algorithms.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Algorithms.Implementation.EmasSupercomputer;
+
+namespace Algorithms.Implementation.Tests
+{
+    [TestClass()]
+    public class EmasSupercomputerTests
+    {
+        [TestMethod()]
+        public void DadosNyMConstruyeMatrizNxM()
+        {
+            int n = 5;
+            int m = 3;
+
+            EmasSupercomputer.Init(n, m);
+
+            Assert.AreEqual(15, EmasSupercomputer.Matrix.Length);
+        }
+
+        [TestMethod()]
+        public void DadaUnaLineaRellenaEsaLinea()
+        {
+            int linea = 1;
+            string datosLinea = "GBB";
+            EmasSupercomputer.Init(3, 3);
+
+            EmasSupercomputer.FillLine(linea, datosLinea);
+
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 0]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[1, 1]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[1, 2]);
+        }
+
+        [TestMethod()]
+        public void DadaTodasLineasRellenaMatriz()
+        {
+            string datosLinea0 = "GBG";
+            string datosLinea1 = "GGG";
+            string datosLinea2 = "GGB";
+            EmasSupercomputer.Init(3, 3);
+
+            EmasSupercomputer.FillLine(0, datosLinea0);
+            EmasSupercomputer.FillLine(1, datosLinea1);
+            EmasSupercomputer.FillLine(2, datosLinea2);
+
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[0, 0]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[0, 1]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[0, 2]);
+
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 0]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 1]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 2]);
+
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[2, 0]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[2, 1]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[2, 2]);
+        }
+
+        private static void InicializarSampleMatrix()
+        {
+            EmasSupercomputer.Init(5, 5);
+            EmasSupercomputer.FillLine(0, "BGGGG");
+            EmasSupercomputer.FillLine(1, "BGGBG");
+            EmasSupercomputer.FillLine(2, "GGGGG");
+            EmasSupercomputer.FillLine(3, "GBGGG");
+            EmasSupercomputer.FillLine(4, "GGGGB");
+        }
+
+        [TestMethod()]
+        public void MatrizGGResultado1()
+        {
+            EmasSupercomputer.Init(1, 2);
+            EmasSupercomputer.FillLine(0, "GG");
+
+            int res = EmasSupercomputer.Process();
+
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod()]
+        public void Matriz4x4GResultado5()
+        {
+            EmasSupercomputer.Init(4, 4);
+            EmasSupercomputer.FillLine(0, "GGGG");
+            EmasSupercomputer.FillLine(1, "GGGG");
+            EmasSupercomputer.FillLine(2, "GGGG");
+            EmasSupercomputer.FillLine(3, "GGGG");
+
+            int res = EmasSupercomputer.Process();
+
+            Assert.AreEqual(5, res);
+        }
+
+        [TestMethod()]
+        public void SampleMatrixPlus22TieneSuperficie9()
+        {
+            InicializarSampleMatrix();
+
+            int area = EmasSupercomputer.SetPlusAndGetHisMaximumArea(2, 2);
+
+            Assert.AreEqual(9, area);
+        }
+
+        [TestMethod()]
+        public void SampleMatrixPlus33TieneSuperficie5()
+        {
+            InicializarSampleMatrix();
+
+            int area = EmasSupercomputer.SetPlusAndGetHisMaximumArea(3, 3);
+
+            Assert.AreEqual(5, area);
+        }
+
+        [TestMethod()]
+        public void AlEstablecerUnPlusLasCeldasQueOcupaCuentanComoBadExceptoLaCentralQueContieneElArea()
+        {
+            InicializarSampleMatrix();
+
+            EmasSupercomputer.SetPlusAndGetHisMaximumArea(3, 3);
+
+            Assert.AreEqual(5, EmasSupercomputer.Matrix[3, 3]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[2, 3]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[4, 3]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[3, 2]);
+            Assert.AreEqual(-1, EmasSupercomputer.Matrix[3, 4]);
+        }
+
+        [TestMethod()]
+        public void AlEliminarUnPlusTodoQuedaComoEstaba()
+        {
+            EmasSupercomputer.Init(3, 3);
+            EmasSupercomputer.Matrix[1, 1] = 5;
+            EmasSupercomputer.Matrix[0, 1] = -1;
+            EmasSupercomputer.Matrix[2, 1] = -1;
+            EmasSupercomputer.Matrix[1, 0] = -1;
+            EmasSupercomputer.Matrix[1, 2] = -1;
+
+            EmasSupercomputer.RemovePlus(1, 1);
+
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[0, 1]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 1]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[2, 1]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 0]);
+            Assert.AreEqual(0, EmasSupercomputer.Matrix[1, 2]);
+        }
+
+        [TestMethod()]
+        public void SampleMatrixResultado9()
+        {
+            InicializarSampleMatrix();
+
+            int res = EmasSupercomputer.Process();
+
+            Assert.AreEqual(9, res);
+        }
+    }
+}
